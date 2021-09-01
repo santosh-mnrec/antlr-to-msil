5a9efdfcd4b7032b40e2140e0d193c3a9c14879c
Santosh Singh <santosh.ksingh03@gmail.com>
2021-09-01 21:26:31 +0800

    Removed unused code

--- CodeGenerator/CodeBuilder.cs
+++ CodeGenerator/CodeBuilder.cs
@@ -39,18 +39,5 @@ namespace AntlrCodeGenerator
             _result.Clear();
             return r;
         }
-        public void LoadIntegerToStack(string name, Value value)
-        {
-            AppendCodeLine(2, "ldc.i4 " + value);
-            AppendCodeLine(2, "stloc " + name);
-
-        }
-        public void LoadToLocal(string value)
-        {
-            AppendCodeLine(2, "stloc " + value);
-            AppendCodeLine(2, "ldloc " + value);
-
-
-        }
     }
 }
